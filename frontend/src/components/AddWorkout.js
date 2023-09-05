abc9adbc6b94acd657393511aa9be71f8f2e698f
Harris Pyo <HPW9229@gmail.com>
2023-09-04 23:59:05 -0400

    updated package.json file and cleaned up some files

--- frontend/src/components/AddWorkout.js
+++ frontend/src/components/AddWorkout.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 import { useUserAuthContext } from "../hooks/useUserAuthContext";
 
